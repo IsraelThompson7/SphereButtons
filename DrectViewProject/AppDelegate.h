f467875f264c10f666a298215acb48c80ba7358e
15038777234 <joser@josertekiiMac.local>
2013-06-19 15:28:24 +0800

    Initial Commit

---
+++ DrectViewProject/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  DrectViewProject
+//
+//  Created by joser on 13-6-19.
+//  Copyright (c) 2013年 joser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
